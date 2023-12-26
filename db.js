d040ecf5a719cedae62d13c68526f64dead20d77
Corentin_k_ <c.kervagoret@outlook.fr>
2023-12-26 14:24:32 +0100

    modif

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-﻿require('dotenv').config();
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config();
 // Créer une instance Sequelize
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.BD_USER, process.env.DB_PWD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PWD, {
+  host:'localhost',
+  dialect: 'mysql',
 });
 // Tester la connexion à la base de données
 async function testConnection() {
